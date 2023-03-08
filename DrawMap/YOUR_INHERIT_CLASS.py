48b1e1875c1473ca512152d45c7c4cbb3b9c6109
dotis-flasnhing <xuanduy650@gmail.com>
2023-03-09 00:08:31 +0700

    learning

---
+++ DrawMap/YOUR_INHERIT_CLASS.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+a = np.array([[1, 2, 3],
+              [4, 5, 6]])
+b = np.array([[4, 5, 6], [7, 8, 9]])
+
+# print(a + b)
+print(a)
+print("a:", a.shape)
+# shape mean (len a,array of len a) shape(so luong mang, so n trong  mang)
+print("a.ndim:", a.ndim)
+# ndim mean (len a) ndim (so luong mang)
+print("a.size:", a.size)
+print("\n")
+c = np.array([
+    [[12, 11, 10], [2, 1, 3]],
+    [[3, 6, 1], [3, 2, 1]],
+    [[1, 2, 3], [4, 2, 9]]
+])
+print(c)
+print("c.shape:", c.shape)
+# shape mean (len a , len in array, len of array) | shape (so luong mang, trong 1 array co nhieu mang, mang [n])
+print("c.ndim:", c.ndim)
+print("c.size:", c.size)
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
